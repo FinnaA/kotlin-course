acc89eb31f481d47a2b13c96bedf7fe1d764fa62
FinnaA <2042km@gmail.com>
2026-09-17 16:05:52 +0300

    add Homework lesson 05

---
+++ src/main/kotlin/com/stimmax/lessons/lesson05/homeworks/ElvisOperator.kt
@@ -0,0 +1,27 @@
+package com.stimmax.lessons.lesson05.homeworks
+
+fun mainlesson05() {
+
+
+//задача про звук
+    val soundBegin: Double = 100.0
+    val coeff: Double? = 0.73 // может быть null
+    val soundEnds = soundBegin * (coeff ?: 0.5)
+    println(soundEnds)
+
+// задача про стоимость груза
+    val shippingCost: Double = 100.00
+    val cargoValue: Double? = null       // может быть null
+    val insuranceCoeff: Double = 0.005
+    val finalCargoValue = cargoValue ?: 50.0
+    val insuranceCost = finalCargoValue * insuranceCoeff
+    val fullCost = shippingCost + insuranceCost
+    println(fullCost)
+
+
+//задача про давление
+    val pressure: Int? // но может быть null
+    val errorMessage: String = "Данные потеряны"
+    val newPressure = pressure ?: errorMessage
+
+}
